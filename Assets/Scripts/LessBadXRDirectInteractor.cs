37e93c84ebd23c2c6088e1eea352d045b2cfa4be
Mark Peschel <mpeschel10@gmail.com>
2023-06-30 13:02:58 -0400

    Both trigger and grip buttons now perform grip

--- Assets/Scripts/LessBadXRDirectInteractor.cs
+++ Assets/Scripts/LessBadXRDirectInteractor.cs
@@ -5,18 +5,22 @@ using UnityEngine.InputSystem;
 
 public class LessBadXRDirectInteractor : MonoBehaviour
 {
-    [SerializeField] InputActionReference selectReference;
+    [SerializeField] InputActionReference selectReference, actionReference;
     void Awake()
     {
         selectReference.action.performed += OnSelectPerformed;
         selectReference.action.canceled += OnSelectCanceled;
+        actionReference.action.performed += OnActionPerformed;
+        actionReference.action.canceled += OnActionCanceled;
     }
 
-    void OnEnable() { selectReference.action.Enable(); }
-    void OnDisable() { selectReference.action.Disable(); }
+    void OnEnable() { selectReference.action.Enable(); actionReference.action.Enable(); }
+    void OnDisable() { selectReference.action.Disable(); actionReference.action.Disable(); }
 
     HashSet<Collider> activeColliders = new HashSet<Collider>();
-    void OnTriggerEnter(Collider other) { activeColliders.Add(other); }
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("Trigger enter");
+        activeColliders.Add(other); }
     void OnTriggerExit(Collider other) { activeColliders.Remove(other); }
 
     Collider GetNearest(HashSet<Collider> colliders)
@@ -41,16 +45,46 @@ public class LessBadXRDirectInteractor : MonoBehaviour
         public void Ungrab();
     }
     Grabbable grabbed;
-    void OnSelectPerformed(InputAction.CallbackContext context)
+    public int select, action = 0;
+    void OnSelectPerformed(InputAction.CallbackContext context) {
+        // Debug.Log("Selecting");
+        int oldTotal = select + action;
+        select = 1;
+        if (oldTotal == 0) OnGrab();
+    }
+    void OnActionPerformed(InputAction.CallbackContext context) {
+        // Debug.Log("Actioning");
+        int oldTotal = select + action;
+        action = 1;
+        if (oldTotal == 0) OnGrab();
+    }
+    void OnSelectCanceled(InputAction.CallbackContext context) {
+        // Debug.Log("unselecting");
+        int oldTotal = select + action;
+        select = 0;
+        if (oldTotal == 1) OnUngrab();
+    }
+    void OnActionCanceled(InputAction.CallbackContext context) {
+        // Debug.Log("Unactioning");
+        int oldTotal = select + action;
+        action = 0;
+        if (oldTotal == 1) OnUngrab();
+    }
+
+    void OnGrab()
     {
+        Debug.Log("Grabbing");
         Collider grabbedCollider = GetNearest(activeColliders);
         if (grabbedCollider == null) return;
         grabbed = grabbedCollider.GetComponentInParent<Grabbable>();
         grabbed.Grab(transform);
+        Debug.Log("Grab happened");
     }
 
-    void OnSelectCanceled(InputAction.CallbackContext context)
+    void OnUngrab()
     {
+        Debug.Log("Ungrabbing");
         if (grabbed != null) grabbed.Ungrab();
     }
+
 }
