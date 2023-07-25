18bc092a4b06705fdf8bbe722c1acacd6c810549
Mark Peschel <mpeschel10@gmail.com>
2023-07-25 12:50:38 -0400

    Reduce color spectrum from pink to purple

--- Assets/Scripts/PillarColor.cs
+++ Assets/Scripts/PillarColor.cs
@@ -6,7 +6,7 @@ public class PillarColor
     {
         // assume height [0, 1]
         // height *= height;
-        float hue = height * 0.8f; // Looks better if highest point is purple instead of cycling back to red
+        float hue = height * 0.75f; // Looks better if highest point is purple instead of cycling back to red
         float saturation = 0.7f + height * 0.3f;
         float value = 0.25f + height * 0.25f;
         return Color.HSVToRGB(hue, saturation, value);
