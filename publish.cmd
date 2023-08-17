b63a11ce55ec2d1043601b70f2508e26d0d181f1
Mark Peschel <mpeschel10@gmail.com>
2023-08-17 11:26:07 -0400

    Script to publish the quest build

--- publish.cmd
+++ publish.cmd
@@ -1,6 +1,9 @@
+call rsync_to_mpeschel10 Builds/Quest/mcreu-2023.apk /var/www/mcreu/quest-build
+call rsync_to_mpeschel10 Builds/WebGL/ /var/www/mcreu/webgl
+
 cd Builds
 7z a -tzip PC-Build.zip .\PC-Build\*
 cd ..
 
 call rsync_to_mpeschel10 Builds/PC-Build.zip /var/www/mcreu/pc-build
-call rsync_to_mpeschel10 Builds/WebGL/ /var/www/mcreu/webgl
+
